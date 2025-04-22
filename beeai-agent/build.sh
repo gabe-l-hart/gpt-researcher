dbb281d650a41835a7c59a5fb013bd82febd4dc2
Gabe Goodhart <ghart@us.ibm.com>
2025-04-22 15:51:43 -0600

    fix: Add the agent manifest label
    Branch: BeeAIPlugin
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- beeai-agent/build.sh
+++ beeai-agent/build.sh
@@ -2,4 +2,7 @@
 
 project_root=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
 cd $project_root
-docker buildx build . -f beeai-agent/Dockerfile -t gpt-researcher-agent:latest
+docker buildx build . \
+    -f beeai-agent/Dockerfile \
+    -t gpt-researcher-agent:latest \
+    --build-arg MANIFEST_B64=$(cat beeai-agent/agent.yaml | base64)
