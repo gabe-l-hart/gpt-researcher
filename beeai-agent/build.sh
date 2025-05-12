853dcbd1ecfcc6fa38475645998e0a33d0c2ee12
Gabe Goodhart <ghart@us.ibm.com>
2025-05-12 17:51:10 -0600

    build: Fix docker build setup for recent beeai changes and rename
    Branch: GraniteLocal
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- beeai-agent/build.sh
+++ beeai-agent/build.sh
@@ -4,5 +4,4 @@ project_root=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
 cd $project_root
 docker buildx build . \
     -f beeai-agent/Dockerfile \
-    -t gpt-researcher-agent:latest \
-    --build-arg MANIFEST_B64=$(cat beeai-agent/agent.yaml | base64)
+    -t gpt-researcher-agent:latest
